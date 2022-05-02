90fcea1fd84cbaabc009b53023ace8b0b1c29d1c
Gerard A <gerard.a.official@gmail.com>
2022-05-02 08:14:26 +0300

    chore: added fetching types

--- src/pages/portfolio-page/index.tsx
+++ src/pages/portfolio-page/index.tsx
@@ -1,24 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Container, Typography } from '@mui/material';
 import axios from 'axios';
+import Picture from '../../types/pictures';
 
-export type Picture = {
-  id: string,
-  title: string,
-  src: string,
-};
-
-const initialPicture: Picture = {
+const initialPicture = {
   id: 'test',
   title: 'Test',
   src: 'https://images.unsplash.com/photo-1607434472257-d9f8e57a643d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80',
 };
 
 const PortfolioPage: React.FC = () => {
-  const [pictures, setPictures] = useState([initialPicture]);
+  const [pictures, setPictures] = useState<Picture[]>([initialPicture]);
 
   useEffect(() => {
-    axios.get('http://localhost:8000/pictures')
+    axios.get<Picture[]>('http://localhost:8000/pictures')
       .then((res) => {
         setPictures(res.data);
       })
