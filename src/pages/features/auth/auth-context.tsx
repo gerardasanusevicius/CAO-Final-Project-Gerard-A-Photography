9c7a0ba4fc68042f470571880d172a987fdaab79
Gerard A <gerard.a.official@gmail.com>
2022-05-02 14:39:13 +0300

    chore:created-auth-context

--- src/pages/features/auth/auth-context.tsx
+++ src/pages/features/auth/auth-context.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { createContext, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Crudentials, Admin } from '../../../types';
+import useLocalStorage from '../../../hooks/use-local-storage-state';
 
-const AuthProvider = () => (
-  <div>AuthProvider</div>
-);
+export type AuthContextType = {
+  user: null | Admin,
+  loggedIn: boolean,
+  login: (next: string) => void,
+  logout: () => void,
+};
+
+const initialValue: AuthContextType = {
+  user: null,
+  loggedIn: false,
+  login: () => {
+    throw new Error('AuthContext.login is not implemented');
+  },
+  logout: () => {
+    throw new Error('AuthContext.logout is not implemented');
+  },
+};
+
+const AuthContext = createContext(initialValue);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const navigate = useNavigate();
+  const [loggedIn, setLoggedIn] = useLocalStorage<AuthContextType['loggedIn']>('loggedIn', false);
+  const [user, setUser] = useState<AuthContextType['user']>(null);
+
+  const login: AuthContextType['login'] = (crudentials: Crudentials, next) => {
+    setLoggedIn(true);
+    navigate(next);
+  };
+
+  const logout: AuthContextType['logout'] = () => {
+    setLoggedIn(false);
+    navigate('/');
+  };
+
+  const providerValue = useMemo(() => ({
+    user,
+    loggedIn,
+    login,
+    logout,
+  }), [loggedIn, user]);
+
+  return (
+    <AuthContext.Provider value={providerValue}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthProvider;
