ba3ed14578cbd353afac4cfad48a801881459296
Gerard A <gerard.a.official@gmail.com>
2022-05-02 23:01:17 +0300

    chore:bug-fix

--- src/hooks/use-local-storage-state.tsx
+++ src/hooks/use-local-storage-state.tsx
@@ -3,13 +3,13 @@ import { useState, useEffect } from 'react';
 type UseLocalStorage = <T>(name: string, defaultValue: T) =>
 [T, (newValue: T) => void];
 
-const useLocalStorage: UseLocalStorage = (name, initialValue) => {
+const useLocalStorage: UseLocalStorage = (name, defaultValue) => {
   const [value, setValue] = useState(() => {
     const existingValue = localStorage.getItem(name);
 
     return existingValue
-      ? JSON.parse(existingValue) as typeof initialValue
-      : initialValue;
+      ? JSON.parse(existingValue) as typeof defaultValue
+      : defaultValue;
   });
 
   useEffect(() => {
