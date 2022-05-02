99170dc80217af91d965e51d83079bd8a4b2b44f
Gerard A <gerard.a.official@gmail.com>
2022-05-02 22:13:02 +0300

    chore:auth-context-updated-error

--- src/pages/features/auth/auth-context.tsx
+++ src/pages/features/auth/auth-context.tsx
@@ -1,36 +1,39 @@
 import React, { createContext, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Admin } from '../../../types';
+import { Crudentials, Admin } from '../../../types';
 import useLocalStorage from '../../../hooks/use-local-storage-state';
+import AuthService from './auth-service';
 
 export type AuthContextType = {
   user: null | Admin,
   loggedIn: boolean,
-  login: (next: string) => void,
+  error: string | null,
+  clearError: VoidFunction,
+  login: (crudentials: Crudentials, next: string) => void,
   logout: () => void,
 };
 
-const initialValue: AuthContextType = {
-  user: null,
-  loggedIn: false,
-  login: () => {
-    throw new Error('AuthContext.login is not implemented');
-  },
-  logout: () => {
-    throw new Error('AuthContext.logout is not implemented');
-  },
-};
-
-const AuthContext = createContext(initialValue);
+const AuthContext = createContext<AuthContextType>({} as AuthContextType);
 
 export const AuthProvider: React.FC = ({ children }) => {
   const navigate = useNavigate();
   const [loggedIn, setLoggedIn] = useLocalStorage<AuthContextType['loggedIn']>('loggedIn', false);
   const [user, setUser] = useState<AuthContextType['user']>(null);
+  const [error, setError] = useState<AuthContextType['error']>(null);
 
-  const login: AuthContextType['login'] = () => {
-    setLoggedIn(true);
-    navigate('/admin');
+  const login: AuthContextType['login'] = async (crudentials: Crudentials, next) => {
+    if (error) {
+      setError(null);
+    }
+    try {
+      const loggedInAdmin = await AuthService.login(crudentials);
+      setLoggedIn(true);
+      setUser(loggedInAdmin);
+      navigate(next);
+    } catch (err) {
+      const { message } = (err as Error);
+      setError(message);
+    }
   };
 
   const logout: AuthContextType['logout'] = () => {
@@ -38,12 +41,18 @@ export const AuthProvider: React.FC = ({ children }) => {
     navigate('/');
   };
 
+  const clearError: AuthContextType['clearError'] = () => {
+    setError(null);
+  };
+
   const providerValue = useMemo(() => ({
     user,
     loggedIn,
+    error,
+    clearError,
     login,
     logout,
-  }), [loggedIn, user]);
+  }), [loggedIn, user, error]);
 
   return (
     <AuthContext.Provider value={providerValue}>
