0b86ed7c4ccdac5200c231a3e6029e3faa6e78a5
Gerard A <gerard.a.official@gmail.com>
2022-05-03 16:59:29 +0300

    chore:footer-created

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  AppBar,
-  Box,
-} from '@mui/material';
+import Box from '@mui/material';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -10,25 +7,33 @@ import FooterLink from './footer-link';
 import Paragraph from '../paragraph';
 
 const Footer: React.FC = () => (
-  <AppBar
-    position="static"
+  <Box
     sx={{
       bgcolor: '#f7f7f7',
       border: 0,
       boxShadow: 'none',
       maxWidth: '100%',
+      flexDirection: 'row',
       justifyContent: 'space-between',
+      height: '137.5px',
+      display: 'flex',
+      alignItems: 'center',
+      padding: '0 90px',
     }}
   >
     <Paragraph>
       Content of this webpage belongs to Gerardas Anusevicius. Any illegal distribution may lead to legal prosecution
     </Paragraph>
-    <Box>
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'row',
+    }}
+    >
       <FooterLink href="https://www.facebook.com/Geanphotography" target="_blank"><FacebookIcon /></FooterLink>
       <FooterLink href="https://www.instagram.com/gerard.a.photography/" target="_blank"><InstagramIcon /></FooterLink>
       <FooterLink href="https://www.linkedin.com/in/gerardas-anusevičius-783497194/" target="_blank"><LinkedInIcon /></FooterLink>
     </Box>
-  </AppBar>
+  </Box>
 );
 
 export default Footer;
