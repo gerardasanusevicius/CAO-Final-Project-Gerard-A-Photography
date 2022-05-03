abdaec5a1f8a1bd008ff40da0a6dffd0c2e46a12
Gerard A <gerard.a.official@gmail.com>
2022-05-03 16:19:03 +0300

    chore:footer-created

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,30 +1,34 @@
 import React from 'react';
 import {
-  Container,
-  Typography,
+  AppBar,
   Box,
 } from '@mui/material';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FooterLink from './footer-link';
+import Paragraph from '../paragraph';
 
 const Footer: React.FC = () => (
-  <Container>
-    <Typography sx={({ theme }) => ({
-      fontSize: '13.5px',
-      letterSpacing: '1.5px',
-      color: theme.palette.primary.dark,
-    })}
-    >
+  <AppBar
+    position="static"
+    sx={{
+      bgcolor: '#f7f7f7',
+      border: 0,
+      boxShadow: 'none',
+      maxWidth: '100%',
+      justifyContent: 'space-between',
+    }}
+  >
+    <Paragraph>
       Content of this webpage belongs to Gerardas Anusevicius. Any illegal distribution may lead to legal prosecution
-    </Typography>
+    </Paragraph>
     <Box>
       <FooterLink href="https://www.facebook.com/Geanphotography" target="_blank"><FacebookIcon /></FooterLink>
       <FooterLink href="https://www.instagram.com/gerard.a.photography/" target="_blank"><InstagramIcon /></FooterLink>
       <FooterLink href="https://www.linkedin.com/in/gerardas-anusevičius-783497194/" target="_blank"><LinkedInIcon /></FooterLink>
     </Box>
-  </Container>
+  </AppBar>
 );
 
 export default Footer;
--- src/components/main-layout.tsx
+++ src/components/main-layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import Navbar from './navbar';
+import Footer from './footer';
 
 const MainLayout: React.FC = () => (
   <>
@@ -9,6 +10,7 @@ const MainLayout: React.FC = () => (
     <Box component="main">
       <Outlet />
     </Box>
+    <Footer />
   </>
 );
 
